ead7cf257116d7455371feedb923de4c721bd03a
aryaraj132 <aryaraj132@gmail.com>
2022-10-09 11:12:13 +0530

    minor fixes

--- api/authRoutes.js
+++ api/authRoutes.js
@@ -5,33 +5,33 @@ const bcrypt = require("bcryptjs")
 
 router.get("/getemployee", async (req,res)=>{
     if(!req.body) {
-        res.status(400).json({message: 'BadRequest'});
+        res.status(400).json({error: 'BadRequest'}).send();
         return;
     }
     if (!req.session.user) {
-        res.status(400).json({"error":"User not Verified"}).send()
+        res.status(400).json({error:"User not Verified"}).send()
         return;
     }else if(!req.session.user.isAdmin){
-        res.status(400).json({"error":"User not Admin"}).send()
+        res.status(400).json({error:"User not Admin"}).send()
         return;
     }
     try {
         const employee = await User.find({isAdmin:false}, {password:0, __v:0})
         res.status(200).json(employee).send()
         }
-    catch(err){console.error(err);res.status(500).json({"error":"Internal Server Error"}).send()}
+    catch(err){console.error(err);res.status(500).json({error:"Internal Server Error"}).send()}
 })
 
 router.post("/addemployee", async (req,res)=>{
     if(!req.body) {
-        res.status(400).json({message: 'BadRequest'});
+        res.status(400).json({error: 'BadRequest'}).send();
         return;
     }
     if (!req.session.user) {
-        res.status(400).json({"error":"User not Verified"}).send()
+        res.status(400).json({error:"User not Verified"}).send()
         return;
     }else if(!req.session.user.isAdmin){
-        res.status(400).json({"error":"User not Admin"}).send()
+        res.status(400).json({error:"User not Admin"}).send()
         return;
     }
     const { name, email, password, contact, department, joiningDate } = req.body;
@@ -39,7 +39,7 @@ router.post("/addemployee", async (req,res)=>{
     try {
         const Emailuser = await User.findOne({email:email})
         if(Emailuser) {
-        res.status(400).json({"error":"Email already exist"}).send()
+        res.status(400).json({error:"Email already exist"}).send()
         }else{
         const salt = await bcrypt.genSalt(10);
         const hashedPass = await bcrypt.hash(req.body.password, salt)
@@ -57,23 +57,23 @@ router.post("/addemployee", async (req,res)=>{
         res.status(200).json(other).send()
         }
     }
-    catch(err){console.error(err);res.status(500).json({"error":"Internal Server Error"}).send()}
+    catch(err){console.error(err);res.status(500).json({error:"Internal Server Error"}).send()}
 })
 
 router.post("/login", async (req,res)=>{
     if(!req.body) {
-        res.status(400).json({message: 'BadRequest'});
+        res.status(400).json({error: 'BadRequest'}).send();
         return;
     }
     const { email, password } = req.body;
     try {
         const user = await User.findOne({email: email});
         if(!user){
-            res.status(400).json({"error":"User Not Found"}).send()}
+            res.status(400).json({error:"User Not Found"}).send()}
         else{
         const validatePass = await bcrypt.compare(password, user.password)
             if(!validatePass){
-                res.status(400).json({"error":"Password Did not match"}).send()
+                res.status(400).json({error:"Password Did not match"}).send()
             }
             else if(user.isAdmin || user.isActive){
             const {password, __v,...other} = user._doc
@@ -81,7 +81,7 @@ router.post("/login", async (req,res)=>{
             res.status(200).json(other).send()
             }
             else{
-                res.status(400).json({"error":"User is not active"}).send()
+                res.status(400).json({error:"User is not active"}).send()
             }
         }
     }
--- api/taskRoutes.js
+++ api/taskRoutes.js
@@ -4,11 +4,11 @@ const Task = require("../models/task.js")
 
 router.post("/addtask", async (req,res)=>{
     if(!req.body) {
-        res.status(400).json({message: 'BadRequest'});
+        res.status(400).json({error: 'BadRequest'}).send();
         return;
     }
     if (!req.session.user) {
-        res.status(400).statusMessage="User not Verified";res.send()
+        res.status(400).json({error:"User not Verified"}).send()
         return;
     }
     const { description, type, timeTaken, starTime } = req.body;
@@ -24,27 +24,27 @@ router.post("/addtask", async (req,res)=>{
         const {__v, _id, ...other} = task._doc
         res.status(200).json(other).send()
     }
-    catch(err){console.error(err);res.status(500).statusMessage="Internal Server Error";res.send()}
+    catch(err){console.error(err);res.status(500).json({error:"Internal Server Error"}).send()}
 })
 
 router.get("/gettask/:id", async (req,res)=>{
     if(!req.body) {
-        res.status(400).json({message: 'BadRequest'});
+        res.status(400).json({error: 'BadRequest'}).send();
         return;
     }
     if (!req.session.user) {
-        res.status(400).statusMessage="User not Verified";res.send()
+        res.status(400).json({error:"User not Verified"}).send()
         return;
     }
     if (!(req.session.user.isAdmin || req.session.user._id == req.params.id)) {
-        res.status(400).statusMessage="Invalid Permission";res.send()
+        res.status(400).json({error:"Invalid Permission"}).send()
         return;
     }
     try {
         const task = await Task.find({userID:req.params.id}, {__v:0, _id:0})
         res.status(200).json(task).send()
         }
-    catch(err){console.error(err);res.status(500).statusMessage="Internal Server Error";res.send()}
+    catch(err){console.error(err);res.status(500).json({error:"Internal Server Error"}).send()}
 })
 
 module.exports = router
